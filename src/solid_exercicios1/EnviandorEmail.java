a4e490c8c5a3cabbfdddc9cc05f1bf9d4b987186
adriana.nogueira <adriana.nogueira@zup.com.br>
2025-01-07 08:07:33 -0300

    feat criacao da classe Enviador de email.

---
+++ src/solid_exercicios1/EnviandorEmail.java
@@ -0,0 +1,7 @@
+package solid_exercicios1;
+
+public class EnviandorEmail {
+    public void enviarPorEmail(String relatorio) {
+        System.out.println("Enviando relatorio por email: " + relatorio);
+    }
+}
